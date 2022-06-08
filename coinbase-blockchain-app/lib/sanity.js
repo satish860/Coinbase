9ac9c1343cbdcef0f080e413d93c808ecd1d8133
satish <satish860@gmail.com>
2022-06-08 18:00:52 +0530

    Set up for the project is done

---
+++ coinbase-blockchain-app/lib/sanity.js
@@ -0,0 +1,11 @@
+import SanityClient from '@sanity/client'
+
+export const client = new SanityClient({
+    projectId:'dkk8tz1m',
+    useCdn:false,
+    dataset:'Production',
+    apiVersion:'08-06-2022',
+    useCdn:false,
+    token:
+        'skFAnHUMlKLJXhML3OsX5Y3r7SLOZu3DRg1QRLPoqnA9gOlgn9aVk6SPHLzMtIDd9xhzvLe8g5zmhKFTF9zxBBjLAeNgdPLoQYbtkbvQaq4xTd6ucrG9H5zffpdKzRbaBRW9Dz6LkTBYubVCIVRuBgjVW6hUcozwxWEVJR29rUQ4Wqzitgxr'
+});
